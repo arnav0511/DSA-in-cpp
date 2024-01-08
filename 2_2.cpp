d875b42a0aacd7f7b3550483b324a21e108aeb86
Arnav Saini <105713306+arnav0511@users.noreply.github.com>
2024-01-08 22:34:56 +0530

    Add files via upload

---
+++ 2_2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=1;
+    cout<<a<<endl;
+
+    char b = 'b';
+    cout<<b<<endl;
+
+    bool bl= true;
+    cout<<bl<<endl;
+
+    float f= 0.12;
+    cout<<f<<endl;
+
+    double d=0.123;
+    cout<<d<<endl;
+
+    cout<<"size of a is "<<sizeof(a)<<endl;
+    cout<<"size of b is "<<sizeof(b)<<endl;
+    cout<<"size of f is "<<sizeof(f)<<endl;
+    cout<<"size of d is "<<sizeof(d)<<endl;
+    cout<<"size of bl is "<<sizeof(bl)<<endl;
+
+    return 0;
+}
